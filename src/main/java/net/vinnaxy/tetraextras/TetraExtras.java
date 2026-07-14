81659a5c532539d5898184af162e041a2d75c2c4
Vinnaxy <iainkrukenberg65@gmail.com>
2026-07-14 16:03:32 -0400

    build.gradle update/cleaned up

--- src/main/java/net/vinnaxy/tetraextras/TetraExtras.java
+++ src/main/java/net/vinnaxy/tetraextras/TetraExtras.java
@@ -46,7 +46,6 @@ public class TetraExtras {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
